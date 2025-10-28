e103ad52cce8a1ccf017c205e892fa3235958f6f
Sachin Singh <sachin8174056@gmail.com>
2025-10-28 18:23:19 +0530

    frist

---
+++ Fronted/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between py-5 font-medium">
+      <img src={assets.logo} alt="Logo" className="w-36" />
+    </div>
+  );
+};
+
+export default Navbar;
