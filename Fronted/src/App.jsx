3c9e4c21587b8bdb7c258aad034f416626b16424
Sachin Singh <sachin8174056@gmail.com>
2025-11-04 07:40:38 +0530

    update

--- Fronted/src/App.jsx
+++ Fronted/src/App.jsx
@@ -12,11 +12,13 @@ import Orders from "./pages/Orders";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import SearchBar from "./components/SearchBar";
-import 'react-toastify/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export default function App() {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-     <ToastContainer/>
+      <ToastContainer position="top-center" autoClose={2000} />
       <Navbar />
       <SearchBar />
       <Routes>
