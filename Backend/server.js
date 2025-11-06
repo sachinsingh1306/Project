986a9bf3385ca7edf1be620a967476938d04f45c
Sachin Singh <sachin8174056@gmail.com>
2025-11-06 23:12:44 +0530

    backend

---
+++ Backend/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import multer from "multer";
+import Razorpay from "razorpay";
+import Stripe from "stripe";
+import validator from "validator";
+import cloudinary from "cloudinary";
+import bcrypt from "bcrypt";
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// --------------------- Middlewares ---------------------
+app.use(express.json());
+app.use(cors());
+
+// --------------------- API Endpoints ---------------------
+app.get("/", (req, res) => {
+  res.send("API WORKING ");
+});
+
+// --------------------- Server Start ---------------------
+app.listen(PORT, () => {
+  console.log(`Server started on PORT ${PORT}`);
+});
+
